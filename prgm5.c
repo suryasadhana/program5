8cd3a6bd27a8b9d71b81df92811996ba20489b1c
suryasadhana <suryasadhana74@gmail.com>
2017-04-26 12:05:55 +0530

    Create prgm5.c

---
+++ prgm5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int n1,n2,n3;
+printf("enter the n:");
+scanf("%d","%d","%d",&n1,&n2,&n3);
+if(n1>=n2 && n1>=n3)
+{
+printf("%d",&n1);
+}
+if(n2>=n1 && n2>=n3)
+{
+printf("%d",&n2);
+}
+if(n3>=n1 && n3>=n2)
+{
+print("%d",&n3);
+getch();
+}
